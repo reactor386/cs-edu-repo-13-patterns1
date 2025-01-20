f17887d96086c7ae524f64fa1851798312d0d2a8
reactor386 <reactor386@gmail.com>
2025-01-21 00:12:14 +0300

    Подготовка исходного проекта
     - исходный проект дополнен вызовом метода CalculateInterest

--- Task1/Task1.App/Program.cs
+++ Task1/Task1.App/Program.cs
@@ -8,6 +8,18 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Calculator");
+        Console.WriteLine("---");
+        int result = 0;
+
+        Account account = new() { Type = "Обычный", Balance = 100 };
+        Calculator.CalculateInterest(account);
+
+        Console.WriteLine($"{account.Type}");
+        Console.WriteLine($"{account.Interest}");
+
+        Console.WriteLine("---");
+        Console.WriteLine($"return: [{result}]");
+        Console.ReadKey();
     }
 }
